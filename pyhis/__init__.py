09c45be3d663d64ee8813f156a6c3e7f2bb03732
wilsaj <wilson.andrew.j+github@gmail.com>
2011-04-07 18:12:33 -0500

    add support for automatically fetching and parsing site info and timeseries/variable information

--- pyhis/__init__.py
+++ pyhis/__init__.py
@@ -20,17 +20,92 @@ class Site(object):
     Contains information about a site
     """
 
+    _series = None
+    _site_info = None
+
     def __init__(self, name=None, code=None, id=None, network=None,
-                 location=None):
+                 location=None, suds_client=None):
         self.name = name
         self.code = code
         self.id = id
         self.network = network
         self.location = util._get_shapely_from_geolocation(location)
+        self.suds_client = suds_client
+
+    @property
+    def site_info(self):
+        if not self._site_info:
+            self._update_site_info()
+        return self._site_info
+
+    @property
+    def variables(self):
+        if not self._series:
+            self._update_site_info()
+        return [series.variable for series in self._series]
+
+    def _update_site_info(self):
+        """makes a GetSiteInfo updates site info and series information"""
+        self._site_info = self.suds_client.service.GetSiteInfoObject(
+            '%s:%s' % (self.network, self.code))
+
+        if len(self._site_info.site) > 1 or \
+               len(self._site_info.site[0].seriesCatalog) > 1:
+            raise NotImplementedError(
+                "Multiple site instances or multiple seriesCatalogs not "
+                "currently supported")
+
+        series_list = self._site_info.site[0].seriesCatalog[0].series
+        self._series = [util._get_series_from_waterml(series)
+                        for series in series_list]
 
     def __repr__(self):
-        return "<Site: %s [%s]>" % (self.name,
-                                    self.code)
+        return "<Site: %s [%s]>" % (self.name, self.code)
+
+
+class Series(object):
+    """
+    Contains information about a time series
+    """
+
+    def __init__(self, variable=None, count=None, method=None,
+                 quality_control_level=None, begin_datetime=None,
+                 end_datetime=None):
+        self.variable = variable
+        self.count = count
+        self.method = method
+        self.quality_control_level = quality_control_level
+        self.begin_datetime = begin_datetime
+        self.end_datetime = end_datetime
+
+
+class Variable(object):
+    """
+    Contains information about a variable
+    """
+
+    def __init__(self, name=None, code=None, id=None, vocabulary=None,
+                 units=None, no_data_value=None):
+        self.name = name
+        self.code = code
+        self.id = id
+        self.vocabulary = vocabulary
+        self.units = units
+        self.no_data_value = no_data_value
+
+    def __repr__(self):
+        return "<Variable: %s [%s]>" % (self.name, self.code)
+
+
+class Units(object):
+    """
+    Contains information about units of measurement
+    """
+
+    def __init__(self, name=None, abbreviation=None, code=None):
+        self.name = name
+        self.abbreviation = abbreviation
+        self.code = code
 
 
 class Client(object):
@@ -42,5 +117,5 @@ class Client(object):
         self.suds_client = suds.client.Client(wsdl_url)
 
         get_all_sites_query = self.suds_client.service.GetSites('')
-        self.sites = [util._get_site_from_site_info(site)
+        self.sites = [util._get_site_from_site_info(site, self.suds_client)
                       for site in get_all_sites_query.site]
--- pyhis/util.py
+++ pyhis/util.py
@@ -5,6 +5,7 @@
     Set of utility functions that help pyhis do its thing.
 
 """
+from datetime import datetime
 
 from shapely.geometry import Point, Polygon
 
@@ -19,20 +20,60 @@ def _get_shapely_from_geolocation(geolocation):
                      geolocation.geogLocation.latitude)
 
     else:
-        raise NotImplementedError("Don't know how to convert location "
-                                  "type: '%s'" %
-                                  geolocation.geogLocation.__class__.__name__)
+        raise NotImplementedError(
+            "Don't know how to convert location type: '%s'" %
+            geolocation.geogLocation.__class__.__name__)
 
 
-def _get_site_from_site_info(site):
+def _get_site_from_site_info(site, suds_client):
     """returns a PyHIS Site instance from a suds WaterML siteInfo element"""
     if len(site.siteInfo.siteCode) > 1:
-        raise NotImplementedError("Multiple site codes not supported")
+        raise NotImplementedError(
+            "Multiple site codes not currently supported")
 
     site_code = site.siteInfo.siteCode[0]
 
-    return pyhis.Site(name=site.siteInfo.siteName,
-                      code=site_code.value,
-                      id=site_code._siteID,
-                      network=site_code._network,
-                      location=site.siteInfo.geoLocation)
+    return pyhis.Site(
+        name=site.siteInfo.siteName,
+        code=site_code.value,
+        id=site_code._siteID,
+        network=site_code._network,
+        location=site.siteInfo.geoLocation,
+        suds_client=suds_client)
+
+
+def _get_variable_from_variable_info(variable_info):
+    """returns a PyHIS Variable instance from a suds WaterML variableInfo
+    element"""
+    if len(variable_info.variableCode) > 1:
+        raise NotImplementedError(
+            "Multiple variable codes not currently supported")
+
+    return pyhis.Variable(
+        name=variable_info.variableName,
+        code=variable_info.variableCode[0].value,
+        id=variable_info.variableCode[0]._variableID,
+        vocabulary=variable_info.variableCode[0]._vocabulary,
+        units=_get_units_from_waterml(variable_info.units),
+        no_data_value=variable_info.NoDataValue)
+
+
+def _get_units_from_waterml(units):
+    """returns a PyHIS Units instance from a suds WaterML units element"""
+    return pyhis.Units(
+        name=units.value,
+        abbreviation=units._unitsAbbreviation,
+        code=units._unitsCode)
+
+
+def _get_series_from_waterml(series):
+    """returns a PyHIS series instance from a suds WaterML series element"""
+    datetime_fmt = "%Y-%d-%m %H:%M:%S"
+
+    return pyhis.Series(
+        variable=_get_variable_from_variable_info(series.variable),
+        count=series.valueCount,
+        method=series.Method.MethodDescription,
+        quality_control_level=series.QualityControlLevel._QualityControlLevelID,
+        begin_datetime=series.variableTimeInterval.beginDateTime,
+        end_datetime=series.variableTimeInterval.endDateTime)
