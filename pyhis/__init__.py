51ef83ad81b9799f39d903f1987c0d8a162af348
wilsaj <wilson.andrew.j+github@gmail.com>
2012-05-30 18:20:25 -0500

    drop a few things from pyhis namespace (note: this breaks a few things and they will need repairing)

--- pyhis/__init__.py
+++ pyhis/__init__.py
@@ -14,19 +14,19 @@ from __future__ import absolute_import
 from .core import *
 
 from . import his_central
-from . import usgs
+#from . import usgs
 
-import quantities as pq
-from . import quantities as phq
+#import quantities as pq
+#from . import quantities as phq
 from . import util
 
 #: mapping of variable codes to quantities they represent
-unit_quantities = {
-    '48': phq.ftH2O,
-    '52': phq.mH2O,
-    '96': pq.degC,
-    '137': pq.dimensionless,
-    '199': phq.mgl,
-    '269': phq.mScm,
-    '306': phq.ppt,
-    }
+#unit_quantities = {
+    #'48': phq.ftH2O,
+    #'52': phq.mH2O,
+    #'96': pq.degC,
+    #'137': pq.dimensionless,
+    #'199': phq.mgl,
+    #'269': phq.mScm,
+    #'306': phq.ppt,
+    #}
