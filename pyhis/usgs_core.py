01050a1b1f0a079d77ad314a5ec496b5443331cb
wilsaj <wilson.andrew.j+github@gmail.com>
2012-05-31 18:34:19 -0500

    clean up core USGS parsing logic to return full variable dicts along with values

--- pyhis/usgs_core.py
+++ pyhis/usgs_core.py
@@ -42,7 +42,7 @@ def get_sites(state_code, site_type=None, service="daily"):
     return sites
 
 
-def get_site_data(service_url, site_code, parameter_code=None,
+def get_site_data(site_code, service=None, parameter_code=None,
                   date_range=None, modified_since=None):
     """queries service for data and returns a data dict"""
     url_params = {'format': 'waterml',
@@ -52,23 +52,56 @@ def get_site_data(service_url, site_code, parameter_code=None,
     if modified_since:
         url_params['modifiedSince'] = isodate.duration_isoformat(modified_since)
 
-    url_params.update(_date_range_url_params(date_range, service_url))
+    if service in ('daily', 'instantaneous'):
+        return _get_site_values(service, date_range, url_params)
+    elif not service:
+        values = _get_site_values('daily', date_range, url_params)
+        values.update(
+            _get_site_values('instantaneous', date_range, url_params))
+        return values
+    else:
+        raise ValueError("service must either be 'daily', 'instantaneous' or none")
+
+
+def _get_site_values(service, date_range, url_params):
+    """downloads and parses values for a site
+
+    returns a values dict containing variable and data values
+    """
+    url_params.update(_date_range_url_params(date_range, service))
+    service_url = _get_service_url(service)
 
     req = requests.get(service_url, params=url_params)
-    print "processing data from request: %s" % req.request.full_url
+    log.info("processing data from request: %s" % req.request.full_url)
 
     if req.status_code != 200:
         # try again with period of 120 days if full range doesn't work
-        if date_range == 'all':
+        if service == 'instantaneous' and date_range == 'all':
             date_range = datetime.timedelta(days=120)
-            return get_site_data(service_url, site_code, parameter_code, date_range, modified_since)
+            return _get_site_values(service, date_range, url_params)
         else:
             return {}
     content_io = StringIO.StringIO(str(req.content))
-    return _parse_site_data_from_waterml(content_io, service_url)
+
+    data_dict = {}
+    for (event, ele) in iterparse(content_io):
+        if ele.tag == NS + "timeSeries":
+            values_element = ele.find(NS + 'values')
+            values = _parse_values(values_element)
+            var_element = ele.find(NS + 'variable')
+            variable = _parse_variable(var_element)
+            code = variable['code']
+            if 'statistic' in variable:
+                code += ":" + variable['statistic']['code']
+            data_dict[code] = {
+                'values': values,
+                'variable': variable,
+            }
+
+    return data_dict
 
 
-def _date_range_url_params(date_range, url):
+def _date_range_url_params(date_range, service):
     """returns a dict of url parameters that should be used for the
     date_range, depending on what type of object date_range is. If
     date_range is a single datetime, returns startDT. If date_range is
@@ -90,9 +123,9 @@ def _date_range_url_params(date_range, url):
         return dict(period=isodate.duration_isoformat(date_range))
         #return dict(startDT=isodate.datetime_isoformat(dt.now() - date_range))
     if date_range == 'all':
-        if 'iv' in url:
+        if service in ('iv', 'instantaneous'):
             return dict(startDT=isodate.date_isoformat(datetime.datetime(2007, 10, 1)))
-        if 'dv' in url:
+        if service in ('dv', 'daily'):
             return dict(startDT=isodate.datetime_isoformat(datetime.datetime(1851, 1, 1)))
 
     raise(TypeError,
@@ -127,19 +160,6 @@ def _parse_geog_location(geog_location):
     }
 
 
-def _parse_site_data_from_waterml(content_io, service_url):
-    data_dict = {}
-
-    for (event, ele) in iterparse(content_io):
-        if ele.tag == NS + "timeSeries":
-            values_element = ele.find(NS + 'values')
-            values = _parse_values(values_element)
-            var_element = ele.find(NS + 'variable')
-            code = var_element.find(NS + 'variableCode').text
-            data_dict[code] = values
-    return data_dict
-
-
 def _parse_site_info(site_info):
     """returns a dict representation of a site given an etree sourceInfo object
     representing a site
@@ -151,13 +171,13 @@ def _parse_site_info(site_info):
     return {
         'agency': site_code.attrib.get('agencyCode'),
         'code': site_code.text,
-        'county': site_info.find(NS + "siteProperty" + "[@name='countyCd']").text,
-        'huc': site_info.find(NS + "siteProperty" + "[@name='hucCd']").text,
+        'county': site_info.find(NS + "siteProperty[@name='countyCd']").text,
+        'huc': site_info.find(NS + "siteProperty[@name='hucCd']").text,
         'location': _parse_geog_location(geog_location),
         'name': site_info.find(NS + "siteName").text,
         'network': site_code.attrib.get('network'),
-        'site_type': site_info.find(NS + "siteProperty" + "[@name='siteTypeCd']").text,
-        'state_code': site_info.find(NS + "siteProperty" + "[@name='stateCd']").text,
+        'site_type': site_info.find(NS + "siteProperty[@name='siteTypeCd']").text,
+        'state_code': site_info.find(NS + "siteProperty[@name='stateCd']").text,
         'timezone_info': _parse_timezone_info(timezone_info),
     }
 
@@ -180,8 +200,8 @@ def _parse_timezone_element(timezone_element):
     defaultTimeZone or daylightSavingsTimeZone)
     """
     return {
-        'offset': timezone_element.attrib.get('zoneOffset'),
         'abbreviation': timezone_element.attrib.get('zoneAbbreviation'),
+        'offset': timezone_element.attrib.get('zoneOffset'),
     }
 
 
@@ -190,7 +210,31 @@ def _parse_values(values_element):
     values element
     """
 
-    return [{'datetime_utc': _parse_datetime(value.attrib['dateTime']),
+    return [{'datetime': _parse_datetime(value.attrib['dateTime']),
              'value': value.text,
              'qualifiers': value.attrib['qualifiers']}
             for value in values_element.findall(NS + 'value')]
+
+
+def _parse_variable(variable_element):
+    """returns a dict that represents a variable for a given etree variable element"""
+    variable_code = variable_element.find(NS + 'variableCode')
+    statistic = variable_element.find(NS + 'options/' + NS + "option[@name='Statistic']")
+    return_dict = {
+        'code': variable_code.text,
+        'description': variable_element.find(NS + 'variableDescription').text,
+        'id': variable_code.attrib.get('variableID'),
+        'network': variable_code.attrib.get('network'),
+        'name': variable_element.find(NS + 'variableName').text,
+        'unit': variable_element.find(NS + 'unit/' + NS + 'unitCode').text,
+        'no_data_value': variable_element.find(NS + 'noDataValue').text,
+        'vocabulary': variable_code.attrib.get('vocabulary'),
+    }
+
+    if statistic is not None:
+        return_dict['statistic'] = {
+            'code': statistic.attrib.get('optionCode'),
+            'name': statistic.text,
+        }
+
+    return return_dict
