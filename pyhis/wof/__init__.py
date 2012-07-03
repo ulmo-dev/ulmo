a6727888ef24039378b67c074259f0742e2e314b
wilsaj <wilson.andrew.j+github@gmail.com>
2012-07-03 12:51:18 -0500

    add skeleton for updated wof backend, following conventions of usgs

---
+++ pyhis/wof/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+
+from . import core
+from . import pytables
---
+++ pyhis/wof/core.py
@@ -0,0 +1,9 @@
+
+
+def get_sites(state_code, site_type=None, service=None):
+    pass
+
+
+def get_site_data(site_code, service=None, parameter_code=None,
+                  date_range=None, modified_since=None):
+    pass
