6f8851e0ce2d7f768054b200d77ee3abe716ea95
wilsaj <wilson.andrew.j+github@gmail.com>
2012-07-21 18:50:01 -0500

    return sites from wof.core.get_sites()

--- pyhis/wof/core.py
+++ pyhis/wof/core.py
@@ -4,13 +4,14 @@ import suds
 
 import pyhis.waterml.v1_0 as wml
 
+
 def get_sites(wsdl_url):
     suds_client = suds.client.Client(wsdl_url)
     suds_client.service.GetSites('')
     response_text = unicode(suds_client.last_received())
     response_buffer = StringIO.StringIO()
 
-    # hacks!
+    # hacks: Hydroserver doesn't declare soap namespace so it doesn't validate
     inject_namespaces = ['soap', 'wsa', 'wsse', 'wsu', 'xsi']
     response_buffer.write(response_text[:53])
     for inject_namespace in inject_namespaces:
@@ -18,8 +19,7 @@ def get_sites(wsdl_url):
     response_buffer.write(response_text[53:])
     response_buffer.seek(0)
     sites = wml.parse_sites(response_buffer)
-    print sites
-
+    return sites
 
 def get_site_data(site_code, service=None, parameter_code=None,
                   date_range=None, modified_since=None):
