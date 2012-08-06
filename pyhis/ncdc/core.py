401ca1fe8afd31a5f4348b2472a1ca004dac2848
wilsaj <wilson.andrew.j+github@gmail.com>
2012-08-06 14:18:26 -0500

    pull and process ncdc stations list from web or cached copy of stations file

--- pyhis/ncdc/core.py
+++ pyhis/ncdc/core.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 import os
 
@@ -6,6 +7,7 @@ import requests
 from pyhis import util
 
 NCDC_GSOD_DIR = os.path.join(util._get_pyhis_dir(), 'ncdc_gsod')
+NCDC_GSOD_STATIONS_FILE = os.path.join(NCDC_GSOD_DIR, 'ish-history.csv')
 NCDC_GSOD_START_YEAR = 1929
 
 
@@ -16,7 +18,7 @@ def download_gsod_files(historical=False, start_year=None, end_year=None):
     ----------
     start_year : year to start downloads. default to current year
     end_year : year to end downloads. default to current year.
-    historical : retrieves all available data. i.e start_year=1928, end_year = current year
+    historical : retrieves all available data. i.e start_year=1928, end_year=current year
 
     default retrieves current year. NCDC GSOD data is updated daily
     """
@@ -48,6 +50,62 @@ def download_gsod_files(historical=False, start_year=None, end_year=None):
                 f.write(chunk)
 
         print 'file saved at {0}'.format(filename)
+
+
+def download_stations_file():
+    """download current station list and return filename
+    """
+    url = 'http://www1.ncdc.noaa.gov/pub/data/gsod/ish-history.csv'
+    r = requests.get(url)
+    with open(NCDC_GSOD_STATIONS_FILE, 'wb') as f:
+        f.write(r.content)
+    print 'Saved station list {0}'.format(NCDC_GSOD_STATIONS_FILE)
+
+
+def get_stations_list(update=True):
+    """returns a dict of station dicts
+
+    stations are keyed to their USAF-WBAN codes
+
+
+    Parameters
+    ----------
+    update : if False, tries to use a cached copy of the stations file. If one
+             can't be found or if update is True, then a new copy of the
+             stations file is pulled from the web.
+    """
+    if update or not os.path.exists(NCDC_GSOD_STATIONS_FILE):
+        download_stations_file()
+
+    with open(NCDC_GSOD_STATIONS_FILE, 'rb') as f:
+        reader = csv.DictReader(f)
+        stations = {
+            '-'.join([row['USAF'], row['WBAN']]): _process_station(row)
+            for row in reader
+        }
+    return stations
+
+
+def _process_station(station_row):
+    """converts a csv row to a more human-friendly version"""
+    station_dict = {
+        'begin': station_row['BEGIN'],
+        'call': station_row['CALL'],
+        'country': station_row['CTRY'],
+        'elevation': float(station_row['ELEV(.1M)']) * .1 \
+                if station_row['ELEV(.1M)'] not in ('', '-99999') else None,
+        'end': station_row['END'],
+        'FIPS': station_row['FIPS'],
+        'latitude': float(station_row['LAT']) * 0.001 \
+                if station_row['LAT'] not in ('', '-99999') else None,
+        'longitude': float(station_row['LON']) * 0.001 \
+                if station_row['LON'] not in ('', '-999999') else None,
+        'name': station_row['STATION NAME'],
+        'state': station_row['STATE'],
+        'USAF': station_row['USAF'],
+        'WBAN': station_row['WBAN'],
+    }
+    return station_dict
 
 
 def _init_temp_dir():
