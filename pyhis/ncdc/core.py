e63214fb33bc306eac8661e3a8c2bacef9fec048
wilsaj <wilson.andrew.j+github@gmail.com>
2012-08-07 19:04:35 -0500

    remove download_gsod_files() function; it's implemented in _download_gsod_file()

--- pyhis/ncdc/core.py
+++ pyhis/ncdc/core.py
@@ -15,47 +15,6 @@ NCDC_GSOD_STATIONS_FILE = os.path.join(NCDC_GSOD_DIR, 'ish-history.csv')
 NCDC_GSOD_START_YEAR = 1929
 
 
-def download_gsod_files(historical=False, start_year=None, end_year=None):
-    """downloads Global Summary of Day data from ncdc and saves files in data directory
-
-    Parameters
-    ----------
-    start_year : year to start downloads. default to current year
-    end_year : year to end downloads. default to current year.
-    historical : retrieves all available data. i.e start_year=1928, end_year=current year
-
-    default retrieves current year. NCDC GSOD data is updated daily
-    """
-    _init_temp_dir()
-    if historical:
-        start_year = NCDC_GSOD_START_YEAR
-        end_year = datetime.datetime.now().year
-
-    if not end_year:
-        end_year = datetime.datetime.now().year
-
-    if not start_year:
-        start_year = datetime.datetime.now().year
-
-    base_url = 'http://www1.ncdc.noaa.gov/pub/data/gsod/'
-    CHUNK = 64 * 1024
-    for year in range(start_year, end_year + 1):
-        url = base_url + '/' + str(year) + '/' + 'gsod_' + str(year) + '.tar'
-        filename = os.path.join(NCDC_GSOD_DIR, url.split('/')[-1])
-        print 'retrieving ncdc gsod tar data file for {0}'.format(year)
-        r = requests.get(url)
-        download = r.raw
-        with open(filename, 'wb') as f:
-            # https://gist.github.com/1311816
-            while True:
-                chunk = download.read(CHUNK)
-                if not chunk:
-                    break
-                f.write(chunk)
-
-        print 'file saved at {0}'.format(filename)
-
-
 def download_stations_file():
     """download current station list and return filename
     """
