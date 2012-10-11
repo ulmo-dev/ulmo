089839cfc6ec9646a7447c441b83e305f2f1a3be
wilsaj <wilson.andrew.j+github@gmail.com>
2012-10-11 17:23:42 -0500

    rm deprecated util function

--- ulmo/util.py
+++ ulmo/util.py
@@ -15,8 +15,6 @@ import pandas
 import requests
 import tables
 
-import pyhis
-
 
 def get_default_h5file_path():
     default_dir = get_pyhis_dir()
@@ -27,37 +25,6 @@ def get_pyhis_dir():
     return_dir = appdirs.user_data_dir('pyhis', 'pyhis')
     mkdir_if_doesnt_exist(return_dir)
     return return_dir
-
-
-def get_parameter_within_polygon(wsdl_list, param_code_list, verts, merge='BySource'):
-    """given a list of service wsdl urls and list of service specific
-    param codes and a list of polygon vertices returns pandas.DataFrame of data.
-    optional flags
-        merge = False : Each site timeseries is a column in dataframe
-        merge = 'BySource' : each column in dataframe corresponds to a service
-        merge = True : single pandas.timeseries
-
-    Note: merging averages across values that occur at the same timestamp.
-    """
-
-    dataframe = pandas.DataFrame()
-    for wsdl_url, param_code in zip(wsdl_list, param_code_list):
-        source = pyhis.Source(wsdl_url)
-        sites = source.get_sites_within_polygon(verts)
-        ts_dict = {}
-        for site in sites:
-            if param_code in sites.timeseries:
-                ts_dict[''.join((ts.site.network,':',ts.site.code))] = ts.data
-                #ts_list.append(site.timeseries[param_code])
-        pd = pandas.Dataset(ts_dict)
-        if merge.lower()=='BySource':
-            pd = pandas.DataSet({site.network:pd.mean(axis=1)})
-        dataframe.append(pd)
-
-    if merge is True:
-        dataframe = dataframe.mean(axis=1)
-
-    return dataframe
 
 
 def get_or_create_group(h5file, path, title):
