d77ebb722e96b7e5ba8d8e61343d375c749a54e6
wilsaj <wilson.andrew.j+github@gmail.com>
2012-10-11 17:04:26 -0500

    cleanup init; remove cruft and imports of deprecated code

--- ulmo/__init__.py
+++ ulmo/__init__.py
@@ -6,24 +6,8 @@
 """
 from __future__ import absolute_import
 
-from .core import *
-
 from . import his_central
 from . import ncdc
 from . import usgs
-from . import wof
-
-#import quantities as pq
-#from . import quantities as phq
 from . import util
-
-#: mapping of variable codes to quantities they represent
-#unit_quantities = {
-    #'48': phq.ftH2O,
-    #'52': phq.mH2O,
-    #'96': pq.degC,
-    #'137': pq.dimensionless,
-    #'199': phq.mgl,
-    #'269': phq.mScm,
-    #'306': phq.ppt,
-    #}
+from . import wof
