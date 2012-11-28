60b881ca4f3ea5c52d6ca1f130f3304f698579b8
wilsaj <wilson.andrew.j+github@gmail.com>
2012-11-28 12:13:55 -0600

    mock out the ghcn daily tests to use a test ghcnd-stations.txt file

--- test/ncdc_ghcn_daily_test.py
+++ test/ncdc_ghcn_daily_test.py
@@ -1,21 +1,27 @@
 from ulmo.ncdc import ghcn_daily
 
+import test_util
+
 
 def test_get_stations_as_dicts():
-    stations = ghcn_daily.core.get_stations()
+    with test_util.mocked_requests('ghcnd-stations.txt'):
+        stations = ghcn_daily.core.get_stations()
     assert len(stations) > 80000
 
 
 def test_get_stations_as_dataframe():
-    stations = ghcn_daily.core.get_stations(as_dataframe=True)
+    with test_util.mocked_requests('ghcnd-stations.txt'):
+        stations = ghcn_daily.core.get_stations(as_dataframe=True)
     assert len(stations) > 80000
 
 
 def test_get_stations_by_country():
-    stations = ghcn_daily.core.get_stations(country='US', as_dataframe=True)
+    with test_util.mocked_requests('ghcnd-stations.txt'):
+        stations = ghcn_daily.core.get_stations(country='US', as_dataframe=True)
     assert 45000 < len(stations) < 46000
 
 
 def test_get_stations_by_state():
-    stations = ghcn_daily.core.get_stations(state='TX', as_dataframe=True)
+    with test_util.mocked_requests('ghcnd-stations.txt'):
+        stations = ghcn_daily.core.get_stations(state='TX', as_dataframe=True)
     assert 3200 < len(stations) < 3300
