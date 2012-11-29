91dad68536761c85f73168cd7415254930e30f0c
wilsaj <wilson.andrew.j+github@gmail.com>
2012-11-29 11:39:12 -0600

    test that ghcn station data are being correctly pulled from the stations file

--- test/ncdc_ghcn_daily_test.py
+++ test/ncdc_ghcn_daily_test.py
@@ -3,16 +3,53 @@ from ulmo.ncdc import ghcn_daily
 import test_util
 
 
+test_stations = [{
+        'country': 'US',
+        'elevation': 286.5,
+        'gsn_flag': 'GSN',
+        'hcn_flag': 'HCN',
+        'id': 'USW00003870',
+        'latitude': 34.8831,
+        'longitude': -82.2203,
+        'name': 'GREER',
+        'network': 'W',
+        'network_id': '00003870',
+        'state': 'SC',
+        'wm_oid': '72312'
+    }, {
+        'country': 'US',
+        'elevation': 91.1,
+        'gsn_flag': None,
+        'hcn_flag': None,
+        'id': 'USW00003850',
+        'latitude': 31.2667,
+        'longitude': -85.7167,
+        'name': 'CAIRNS FLD FT RUCKER',
+        'network': 'W',
+        'network_id': '00003850',
+        'state': 'AL',
+        'wm_oid': None
+    }]
+
+
 def test_get_stations_as_dicts():
     with test_util.mocked_requests('ghcnd-stations.txt'):
         stations = ghcn_daily.core.get_stations()
     assert len(stations) > 80000
+
+    for test_station in test_stations:
+        station_id = test_station.get('id')
+        assert stations.get(station_id) == test_station
 
 
 def test_get_stations_as_dataframe():
     with test_util.mocked_requests('ghcnd-stations.txt'):
         stations = ghcn_daily.core.get_stations(as_dataframe=True)
     assert len(stations) > 80000
+
+    for test_station in test_stations:
+        station_id = test_station.get('id')
+        assert stations.xs(station_id).to_dict() == test_station
 
 
 def test_get_stations_by_country():
