18f4322b208aa2178e1c827511081e1d2b436549
wilsaj <wilson.andrew.j+github@gmail.com>
2012-12-03 17:40:09 -0600

    add function for getting ghcn data by station id

--- test/ncdc_ghcn_daily_test.py
+++ test/ncdc_ghcn_daily_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas
 
 from ulmo.ncdc import ghcn_daily
@@ -32,6 +33,52 @@ test_stations = [{
         'state': 'AL',
         'wm_oid': None
     }]
+
+test_data = {
+    'USW00003870': {
+        'ACMH': {
+            '1965-03-27': (50, np.nan, np.nan, 'X'),
+        },
+        'PRCP': {
+            '1962-10-01': (np.nan, np.nan, np.nan, np.nan),
+            '1962-10-14': (np.nan, np.nan, np.nan, np.nan),
+            '1962-10-15': (0, np.nan, np.nan, 'X'),
+            '1962-10-16': (0, 'T', np.nan, 'X'),
+            '1962-10-17': (0, np.nan, np.nan, 'X'),
+            '1962-10-21': (102, np.nan, np.nan, 'X'),
+            '1962-10-31': (30, np.nan, np.nan, 'X'),
+            '1963-02-01': (15, np.nan, np.nan, '0'),
+            '1963-02-28': (0, np.nan, np.nan, '0'),
+            '1963-03-01': (249, np.nan, np.nan, '0'),
+            '1984-02-01': (0, np.nan, np.nan, '0'),
+            '1984-02-06': (8, np.nan, np.nan, '0'),
+            '1984-02-28': (20, np.nan, np.nan, '0'),
+            '1984-02-29': (0, np.nan, np.nan, '0'),
+            '1984-03-01': (0, np.nan, np.nan, '0'),
+            '1984-03-05': (287, np.nan, np.nan, '0'),
+            '2012-03-01': (0, 'T', np.nan, 'X'),
+            '2012-03-02': (10, np.nan, np.nan, 'X'),
+            '2012-03-03': (422, np.nan, np.nan, 'X'),
+        },
+    },
+}
+
+
+def test_get_data():
+    for station_id, sample_data in test_data.iteritems():
+        elements = sample_data.keys()
+        station_data = ghcn_daily.core.get_data(station_id, elements=elements)
+
+        for element_id, element_test_data in sample_data.iteritems():
+            element_df = station_data[element_id]
+            for date, test_value in element_test_data.iteritems():
+                value = element_df.xs(date)
+                test_array = np.array(test_value, dtype=value.dtype)
+
+                nulls = pandas.isnull(value)
+
+                assert np.all(pandas.isnull(test_array) == nulls)
+                assert np.all(value[~nulls] == test_array[~nulls])
 
 
 def test_get_stations_as_dicts():
--- ulmo/ncdc/ghcn_daily/core.py
+++ ulmo/ncdc/ghcn_daily/core.py
@@ -1,5 +1,7 @@
+import itertools
 import os
 
+import numpy as np
 import pandas
 
 from ulmo import util
@@ -7,6 +9,66 @@ from ulmo import util
 
 GHCN_DAILY_DIR = os.path.join(util.get_ulmo_dir(), 'ncdc/ghcn_daily')
 GHCN_DAILY_STATIONS_FILE = os.path.join(GHCN_DAILY_DIR, 'ghcnd-stations.txt')
+
+
+def get_data(station_id, elements=None, update=True):
+    if isinstance(elements, basestring):
+        elements = [elements]
+
+    station_file_path = _get_ghcn_file(station_id + '.dly')
+
+    start_columns = [
+        ('year', 11, 15),
+        ('month', 15, 17),
+        ('element', 17, 21),
+    ]
+    value_columns = [
+        ('value', 0, 5),
+        ('mflag', 5, 6),
+        ('qflag', 6, 7),
+        ('sflag', 7, 8),
+    ]
+    columns = list(itertools.chain(start_columns, *[
+        [(name + str(n), start + 13 + (8 * n), end + 13 + (8 * n))
+         for name, start, end in value_columns]
+        for n in xrange(1, 32)
+    ]))
+    colspecs = [(start, end) for name, start, end in columns]
+    names = [name for name, start, end in columns]
+
+    station_data = pandas.io.parsers.read_fwf(station_file_path,
+            colspecs=colspecs, header=None, na_values=['-9999'], names=names)
+
+    dataframes = {}
+
+    for element_name, element_df in station_data.groupby('element'):
+        if not elements is None and element_name not in elements:
+            continue
+
+        year_month = ['%s-%s' % tuple(i)
+                      for i in element_df[['year', 'month']].values]
+        monthly_index = pandas.PeriodIndex(year_month, freq='M')
+        element_df.index = monthly_index
+        # here we're just using pandas' builtin resample logic to construct a daily
+        # index for the timespan
+        daily_index = element_df.resample('D').index.copy()
+
+        # XXX: hackish; pandas support for this sort of thing will probably be
+        # added soon
+        month_starts = (monthly_index - 1).asfreq('D') + 1
+        dataframe = pandas.DataFrame(
+                columns=['value', 'mflag', 'qflag', 'sflag'], index=daily_index)
+
+        for day_of_month in range(1, 32):
+            dates = [date for date in (month_starts + day_of_month - 1)
+                    if date.day == day_of_month]
+            months = [pandas.Period(date, 'M') for date in dates]
+            for column_name in dataframe.columns:
+                col = column_name + str(day_of_month)
+                dataframe[column_name][dates] = element_df[col][months]
+
+        dataframes[element_name] = dataframe
+    return dataframes
 
 
 def get_stations(country=None, state=None, update=True, as_dataframe=False):
@@ -75,3 +137,15 @@ def get_stations(country=None, state=None, update=True, as_dataframe=False):
             stations[column_name][pandas.isnull(stations[column_name])] = None
 
         return stations.T.to_dict()
+
+
+def _get_ghcn_file(filename, check_modified=True):
+    base_url = 'http://www1.ncdc.noaa.gov/pub/data/ghcn/daily/'
+    if 'ghcnd-' in filename:
+        url = base_url + filename
+    else:
+        url = base_url + 'all/' + filename
+
+    path = os.path.join(GHCN_DAILY_DIR, url.split('/')[-1])
+    util.download_if_new(url, path)
+    return path
