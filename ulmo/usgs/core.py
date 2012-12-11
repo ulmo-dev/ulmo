d2d851f2e822245553f222ef63428a907856ee1a
wilsaj <wilson.andrew.j+github@gmail.com>
2012-12-11 12:36:54 -0600

    don't need to guard against poorly mocked requests in usgs/core.py

--- ulmo/usgs/core.py
+++ ulmo/usgs/core.py
@@ -51,8 +51,7 @@ def get_sites(sites=None, state_code=None, site_type=None, service=None):
         url = _get_service_url(service)
         log.info('making request for sites: %s' % url)
         req = requests.get(url, params=url_params)
-        if hasattr(req.request, 'full_url'):
-            log.info("processing data from request: %s" % req.request.full_url)
+        log.info("processing data from request: %s" % req.request.full_url)
         content_io = StringIO.StringIO(str(req.content))
 
         return_sites = wml.parse_sites(content_io)
@@ -136,8 +135,7 @@ def _get_site_values(service, date_range, url_params):
     except requests.exceptions.ConnectionError:
         log.info("There was a connection error with query:\n\t%s\n\t%s" % (service_url, url_params))
         return {}
-    if hasattr(req.request, 'full_url'):
-        log.info("processing data from request: %s" % req.request.full_url)
+    log.info("processing data from request: %s" % req.request.full_url)
 
     if req.status_code != 200:
         # try again with period of 120 days if full range doesn't work
