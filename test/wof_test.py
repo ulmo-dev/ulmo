a68ca80b721e2dc6f1278a49ab29c62bee2a7897
wilsaj <wilson.andrew.j+github@gmail.com>
2013-01-05 16:36:10 -0600

    change wof test file naming scheme

--- test/wof_test.py
+++ test/wof_test.py
@@ -2,6 +2,7 @@ import ulmo
 
 import test_util
 
+def test_core_get_sites_waterml_1_0():
     WSDL_URL = 'http://hydroportal.cuahsi.org/muddyriver/cuahsi_1_0.asmx?WSDL'
     get_sites_file = 'muddyriver_1_0_get_sites.xml'
     with test_util.mocked_suds_client('1.0', dict(GetSitesXml=get_sites_file)):
@@ -11,7 +12,7 @@ import test_util
 
 def test_core_get_sites_waterml_1_1():
     WSDL_URL = 'http://hydroportal.cuahsi.org/ipswich/cuahsi_1_1.asmx?WSDL'
-    get_sites_file = 'ipswich_1_1_get_sites.xml'
+    get_sites_file = 'get_sites_ipswich_1_1.xml'
     with test_util.mocked_suds_client('1.1', dict(GetSites=get_sites_file)):
         sites = ulmo.wof.core.get_sites(WSDL_URL)
     assert len(sites) == 34
