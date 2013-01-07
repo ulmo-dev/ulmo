07e5bab85218c85596b9e9a8d43c4f392d42d499
wilsaj <wilson.andrew.j+github@gmail.com>
2013-01-06 18:43:12 -0600

    update muddyriver get_sites test file

--- test/wof_test.py
+++ test/wof_test.py
@@ -4,7 +4,7 @@ import test_util
 
 def test_core_get_sites_waterml_1_0():
     WSDL_URL = 'http://hydroportal.cuahsi.org/muddyriver/cuahsi_1_0.asmx?WSDL'
-    get_sites_file = 'muddyriver_1_0_get_sites.xml'
+    get_sites_file = 'get_sites_muddyriver_1_0.xml'
     with test_util.mocked_suds_client('1.0', dict(GetSitesXml=get_sites_file)):
         sites = ulmo.wof.core.get_sites(WSDL_URL)
     assert len(sites) == 14
