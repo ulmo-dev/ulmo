8c8b0e0df791e014c66b83159d201cc4d2da04c1
wilsaj <wilson.andrew.j+github@gmail.com>
2013-01-14 10:08:42 -0600

    bump to version 0.3.1-dev

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('README.rst') as f:
 
 setup(
     name='ulmo',
-    version='0.3.0',
+    version='0.3.1-dev',
     license='BSD',
     author='Andy Wilson',
     author_email='wilson.andrew.j@gmail.com',
