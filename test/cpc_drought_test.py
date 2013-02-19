e8bbb73aea5ab69699166dc9fa3c343a0f500b05
wilsaj <wilson.andrew.j+github@gmail.com>
2013-02-19 11:48:13 -0600

    add basic unit test & framework for cpc drought dataset

--- test/cpc_drought_test.py
+++ test/cpc_drought_test.py
@@ -1,21 +1,22 @@
 import ulmo
 
-test_values = [{
-    'state': 'AL',
-    'climate_division': 1,
-    'period': '2013-01-27/2013-02-02',
-    'value': {
-        'cmi': 2.71,
-        'pdsi': 2.54,
+test_values = [
+    {
+        'state': 'AL',
+        'climate_division': 1,
         'period': '2013-01-27/2013-02-02',
-        'potential_evap': 0.15,
-        'precipitation': 1.57,
-        'runoff': 1.42,
-        'soil_moisture_lower': 5.0,
-        'soil_moisture_upper': 1.0,
-        'temperature': 47.4,
-        'z': 2.15
-    }
+        'value': {
+            'cmi': 2.71,
+            'pdsi': 2.54,
+            'period': '2013-01-27/2013-02-02',
+            'potential_evap': 0.15,
+            'precipitation': 1.57,
+            'runoff': 1.42,
+            'soil_moisture_lower': 5.0,
+            'soil_moisture_upper': 1.0,
+            'temperature': 47.4,
+        }
+    },
 ]
 
 
@@ -29,6 +30,9 @@ def test_get_data():
     data = ulmo.cpc.drought.get_data()
 
     for test_value in test_values:
-        import pdb; pdb.set_trace()
-
-    pass
+        values = data.get(test_value['state'], {}).get(test_value['climate_division'])
+        value = [
+            value for value in values
+            if test_value['period'] == value['period']
+        ][0]
+        assert value == test_value['value']
