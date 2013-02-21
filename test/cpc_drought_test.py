cee77aaebb523c2222378bf75a37000cb9d2466b
wilsaj <wilson.andrew.j+github@gmail.com>
2013-02-21 15:30:43 -0600

    clean up cpc.drought tests: use a mocked data file

--- test/cpc_drought_test.py
+++ test/cpc_drought_test.py
@@ -1,38 +1,42 @@
 import ulmo
 
-test_values = [
+import test_util
+
+
+test_sets = [
     {
         'state': 'AL',
         'climate_division': 1,
-        'period': '2013-01-27/2013-02-02',
-        'value': {
-            'cmi': 2.71,
-            'pdsi': 2.54,
-            'period': '2013-01-27/2013-02-02',
-            'potential_evap': 0.15,
-            'precipitation': 1.57,
-            'runoff': 1.42,
-            'soil_moisture_lower': 5.0,
-            'soil_moisture_upper': 1.0,
-            'temperature': 47.4,
-        }
+        'values': [{
+            'cmi': 0.05,
+            'pdsi': 0.53,
+            'period': '2010-06-06/2010-06-12',
+            'potential_evap': 1.37,
+            'precipitation': 0.71,
+            'runoff': 0.0,
+            'soil_moisture_lower': 4.86,
+            'soil_moisture_upper': 0.0,
+            'temperature': 77.6
+        }]
     },
 ]
 
 
 def test_get_data_by_state():
-    data = ulmo.cpc.drought.get_data(state='AL')
+    with test_util.mocked_requests('cpc/drought/palmer10'):
+        data = ulmo.cpc.drought.get_data(state='AL', start='2010-5-20',
+                end='2010-6-13')
     assert len(data) == 1
     assert 'AL' in data
 
 
 def test_get_data():
-    data = ulmo.cpc.drought.get_data()
+    with test_util.mocked_requests('cpc/drought/palmer10'):
+        data = ulmo.cpc.drought.get_data(start='2010-5-20', end='2010-6-13')
 
-    for test_value in test_values:
-        values = data.get(test_value['state'], {}).get(test_value['climate_division'])
-        value = [
-            value for value in values
-            if test_value['period'] == value['period']
-        ][0]
-        assert value == test_value['value']
+    for test_set in test_sets:
+        values = data.get(test_set['state'], {}).get(test_set['climate_division'])
+        test_values = test_set['values']
+
+        for test_value in test_values:
+            assert test_value in values
