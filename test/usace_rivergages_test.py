f4726ea1e72316530f7d6e632eb25863ad78582b
wilsaj <wilson.andrew.j+github@gmail.com>
2013-02-22 13:41:10 -0600

    unit test usace.rivergages.get_stations

---
+++ test/usace_rivergages_test.py
@@ -0,0 +1,11 @@
+import ulmo
+
+import test_util
+
+
+def test_get_stations():
+    stations_file = 'usace/rivergages/datamining2.cfm'
+    with test_util.mocked_requests(stations_file):
+        stations = ulmo.usace.rivergages.get_stations()
+    assert 1900 <= len(stations) <= 2000
+    assert 'CE7F42E6' in stations
