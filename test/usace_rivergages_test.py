98a24128499d99c963b6ea50487e79a5caceab43
wilsaj <wilson.andrew.j+github@gmail.com>
2013-02-22 15:09:55 -0600

    rename rivergages test file

--- test/usace_rivergages_test.py
+++ test/usace_rivergages_test.py
@@ -4,7 +4,7 @@ import test_util
 
 
 def test_get_stations():
-    stations_file = 'usace/rivergages/datamining2.cfm'
+    stations_file = 'usace/rivergages/get_stations.cfm'
     with test_util.mocked_requests(stations_file):
         stations = ulmo.usace.rivergages.get_stations()
     assert 1900 <= len(stations) <= 2000
