e86f408d780aafcf6b959dd286243d59e60534a4
wilsaj <wilson.andrew.j+github@gmail.com>
2013-02-22 13:40:48 -0600

    add a usace.rivergages module for the army corps' rivergages website

--- ulmo/usace/__init__.py
+++ ulmo/usace/__init__.py
@@ -1,1 +1,2 @@
+import rivergages
 import swtwc
---
+++ ulmo/usace/rivergages/core.py
@@ -0,0 +1,25 @@
+import os.path
+
+from bs4 import BeautifulSoup
+
+from ulmo import util
+
+USACE_RIVERGAGES_DIR = os.path.join(util.get_ulmo_dir(), 'usace/rivergages/')
+
+
+def get_stations():
+    url = 'http://rivergages.mvr.usace.army.mil/WaterControl/datamining2.cfm'
+    path = os.path.join(USACE_RIVERGAGES_DIR, 'datamining_field_list.cfm')
+
+    with util.open_file_for_url(url, path) as f:
+        soup = BeautifulSoup(f)
+
+        options = soup.find('select', id='fld_station').find_all('option')
+
+        stations = dict([
+            (option.attrs.get('value'), option.text.strip())
+            for option in options
+            if option.attrs.get('value') != ''
+        ])
+
+    return stations
