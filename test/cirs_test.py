3ed55cb579e0f0a7e70a2e2628f06ccf7a256197
wilsaj <wilson.andrew.j+github@gmail.com>
2013-04-11 12:10:22 -0500

    add tests for ncdc CIRS by-state/region datasets

--- test/cirs_test.py
+++ test/cirs_test.py
@@ -58,11 +58,51 @@ test_sets = [
                 'value': 1.94,
             },
         ]
+    }, {
+        'index': 'tmp',
+        'by_state': True,
+        'values': [
+            {
+                'location': 'ME',
+                'location_code': 17,
+                'year': 1895,
+                'month': 1,
+                'value': 14.00,
+            }, {
+                'location': 'ME',
+                'location_code': 17,
+                'year': 1895,
+                'month': 12,
+                'value': 22.70,
+            }, {
+                'location': 'national',
+                'location_code': 110,
+                'year': 2013,
+                'month': 1,
+                'value': 31.90,
+            }, {
+                'location': 'national',
+                'location_code': 110,
+                'year': 2013,
+                'month': 2,
+                'value': 34.78,
+            },
+        ]
     },
 ]
 
 
+def test_get_data_by_state():
+    state_tests = [s for s in test_sets if s['by_state']]
+    _run_test_sets(state_tests)
+
+
 def test_get_data_by_climate_division():
+    division_tests = [s for s in test_sets if not s['by_state']]
+    _run_test_sets(division_tests)
+
+
+def _run_test_sets(test_sets):
     for test_set in test_sets:
         index = test_set['index']
         by_state = test_set['by_state']
