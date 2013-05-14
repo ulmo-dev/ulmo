c6ecad87b5d33a8b14130f0ce8668df787765bf0
wilsaj <wilson.andrew.j+github@gmail.com>
2013-05-14 14:59:45 -0500

    bump to 0.5.0 for release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open('README.rst') as f:
 
 setup(
     name='ulmo',
-    version='0.4.2-dev',
+    version='0.5.0',
     license='BSD',
     author='Andy Wilson',
     author_email='wilson.andrew.j@gmail.com',
