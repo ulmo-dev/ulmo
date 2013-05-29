c11bcf21d0c26a24d96642d129a18b38f58cb4da
wilsaj <wilson.andrew.j+github@gmail.com>
2013-05-28 22:24:36 -0500

    update cirs tests to handle renamed values

--- test/cirs_test.py
+++ test/cirs_test.py
@@ -6,7 +6,7 @@ import test_util
 
 test_sets = [
     {
-        'index': 'pdsi',
+        'element': 'pdsi',
         'by_state': False,
         'values': [
             {
@@ -15,53 +15,53 @@ test_sets = [
                 'division': 8,
                 'year': 1998,
                 'month': 1,
-                'value': 2.35,
+                'pdsi': 2.35,
             }, {
                 'state': 'WY',
                 'state_code': 48,
                 'division': 8,
                 'year': 1998,
                 'month': 2,
-                'value': 2.31,
+                'pdsi': 2.31,
             }, {
                 'state': 'WY',
                 'state_code': 48,
                 'division': 8,
                 'year': 1998,
                 'month': 3,
-                'value': 2.33,
+                'pdsi': 2.33,
             }, {
                 'state': 'WY',
                 'state_code': 48,
                 'division': 8,
                 'year': 1998,
                 'month': 11,
-                'value': 3.11,
+                'pdsi': 3.11,
             }, {
                 'state': 'WY',
                 'state_code': 48,
                 'division': 8,
                 'year': 1998,
                 'month': 12,
-                'value': 2.96,
+                'pdsi': 2.96,
             }, {
                 'state': 'TX',
                 'state_code': 41,
                 'division': 10,
                 'year': 1895,
                 'month': 1,
-                'value': -0.1,
+                'pdsi': -0.1,
             }, {
                 'state': 'TX',
                 'state_code': 41,
                 'division': 10,
                 'year': 1895,
                 'month': 12,
-                'value': 1.94,
+                'pdsi': 1.94,
             },
         ]
     }, {
-        'index': 'tmp',
+        'element': 'tmp',
         'by_state': True,
         'values': [
             {
@@ -69,29 +69,29 @@ test_sets = [
                 'location_code': 17,
                 'year': 1895,
                 'month': 1,
-                'value': 14.00,
+                'tmp': 14.00,
             }, {
                 'location': 'ME',
                 'location_code': 17,
                 'year': 1895,
                 'month': 12,
-                'value': 22.70,
+                'tmp': 22.70,
             }, {
                 'location': 'national',
                 'location_code': 110,
                 'year': 2013,
                 'month': 1,
-                'value': 31.91,
+                'tmp': 31.91,
             }, {
                 'location': 'national',
                 'location_code': 110,
                 'year': 2013,
                 'month': 2,
-                'value': 34.77,
+                'tmp': 34.77,
             },
         ]
     }, {
-        'index': 'tmp',
+        'element': 'tmp',
         'by_state': True,
         'location_names': 'full',
         'values': [
@@ -100,7 +100,7 @@ test_sets = [
                 'location_code': 17,
                 'year': 1895,
                 'month': 1,
-                'value': 14.00,
+                'tmp': 14.00,
             },
         ]
     },
@@ -118,23 +118,23 @@ def test_get_data_by_climate_division():
 
 
 def test_doesnt_have_locations_if_location_names_is_none():
-    index = 'pdsi'
-    use_file = _test_use_file(index, False)
-    data = ulmo.ncdc.cirs.get_data(index, location_names=None,
-            use_file=use_file, as_dataframe=True)
+    element = 'pdsi'
+    use_file = _test_use_file(element, False)
+    data = ulmo.ncdc.cirs.get_data(
+        element, location_names=None, use_file=use_file, as_dataframe=True)
     assert 'location' not in data.columns
 
 
 def _run_test_sets(test_sets):
     for test_set in test_sets:
         test_args = copy.copy(test_set)
-        index = test_args.pop('index')
+        element = test_args.pop('element')
         test_values = test_args.pop('values')
         by_state = test_args['by_state']
 
-        use_file = _test_use_file(index, by_state)
-        data = ulmo.ncdc.cirs.get_data(index, use_file=use_file,
-                as_dataframe=True, **test_args)
+        use_file = _test_use_file(element, by_state)
+        data = ulmo.ncdc.cirs.get_data(
+            element, use_file=use_file, as_dataframe=True, **test_args)
         for test_value in test_values:
             _assert_inclusion(test_value, data)
 
@@ -148,12 +148,15 @@ def _assert_inclusion(value_dict, dataframe):
     assert len(sub_df) == 1
 
 
-def _test_use_file(index, by_state):
+def _test_use_file(elements, by_state):
     if test_util.use_test_files():
-        if by_state:
-            path = 'ncdc/cirs/drd964x.%sst.txt' % index
+        if isinstance(elements, basestring):
+            if by_state:
+                path = 'ncdc/cirs/drd964x.%sst.txt' % elements
+            else:
+                path = 'ncdc/cirs/drd964x.%s.txt' % elements
         else:
-            path = 'ncdc/cirs/drd964x.%s.txt' % index
+            path = 'ncdc/cirs/'
         return test_util.get_test_file_path(path)
     else:
         return None
