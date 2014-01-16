55791f4cbd08327054dde2638065f73b656c94cb
wilsaj <wilson.andrew.j+github@gmail.com>
2014-01-16 15:12:53 -0600

    explicitly cast list of months to a PeriodIndex object so masking works as expected across supported pandas versions

--- ulmo/ncdc/ghcn_daily/core.py
+++ ulmo/ncdc/ghcn_daily/core.py
@@ -102,7 +102,7 @@ def get_data(station_id, elements=None, update=True, as_dataframe=False):
                     if date.day == day_of_month]
             if not len(dates):
                 continue
-            months = [pandas.Period(date, 'M') for date in dates]
+            months = pandas.PeriodIndex([pandas.Period(date, 'M') for date in dates])
             for column_name in dataframe.columns:
                 col = column_name + str(day_of_month)
                 dataframe[column_name][dates] = element_df[col][months]
