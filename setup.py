0954b666506767240dd73e1f0ca4ea4d21e18388
wilsaj <wilson.andrew.j+github@gmail.com>
2014-05-31 23:42:35 -0500

    pandas 0.14 release seems to be broken for python 2.6(?)

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         # mock is required for mocking pytables-related functionality when it doesn't exist
         'mock>=1.0.0',
         'numpy>=1.4.0',
-        'pandas>=0.11',
+        'pandas<0.14',
         'requests>=1.1',
         'suds>=0.4',
     ],
