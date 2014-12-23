6a13408a8349b4a631eed20b8d79a82a884eb2e9
Dharhas Pothina <dharhas@gmail.com>
2014-12-23 13:10:02 -0600

    make ned api match the eros api more closely

--- ulmo/usgs/ned/__init__.py
+++ ulmo/usgs/ned/__init__.py
@@ -4,5 +4,5 @@
 
     .. _National Elevation Dataset (NED): http://ned.usgs.gov
 """
-from .core import download_tiles, get_available_tiles, get_file_index, get_raster, get_tile_urls
+from .core import download_tiles, get_raster, get_raster_availability
 
