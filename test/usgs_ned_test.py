6fd32ffd1b6fe65ea3d5b3da76078717242965ec
Dharhas Pothina <dharhas@gmail.com>
2015-04-24 11:45:21 -0500

    add tests for usgs.ned

--- test/usgs_ned_test.py
+++ test/usgs_ned_test.py
@@ -1,33 +1,48 @@
 import pytest
 import ulmo
 import test_util
+import tempfile
+import filecmp
 
 
 test_sets = [
 	{'layer': '1 arc-second',
 	 'bbox': (-79.68433821243661, 32.81417227179455, -77.42742156945509, 34.798064728936754),
+	 'number_of_tiles': 7,
+	 'file': 'usgs/ned/availability_bbox_test_set_1.json',
 	},
 	{'layer': '1/3 arc-second',
 	 'bbox': (-79.68433821243661, 32.81417227179455, -77.42742156945509, 34.798064728936754),
+	 'number_of_tiles': 7,
+	 'file': 'usgs/ned/availability_bbox_test_set_2.json',
 	},
 	{'layer': 'Alaska 2 arc-second',
 	 'bbox': (-150.4969, 60.7338, -148.4600, 61.5253),
+	 'number_of_tiles': 6,
+	 'file': 'usgs/ned/availability_bbox_test_set_3.json',
 	},
 ]
 
-def test_get_tile_urls_local():
-	# note: there it looks like ymax is rounded up when greater than .98 
-	# at that point some extra tiles are downloaded. Ignoring this issue 
-	# for now since all the required tiles are being downloaded.
-	# note: 2 arc-second coverage is Alaska only
-	for dataset in test_sets:
-		urls_from_webservice = ulmo.usgs.ned.get_tile_urls(
-			dataset['layer'], 
-			*dataset['bbox'], 
-			use_webservice=True)
-		urls_from_local = ulmo.usgs.ned.get_tile_urls(
-			dataset['layer'], 
-			*dataset['bbox'], 
-			use_webservice=False)
 
-		assert set(urls_from_webservice) == set(urls_from_local)
+def test_get_raster_availability():
+	for dataset in test_sets:
+		with test_util.mocked_urls(dataset['file']):
+			locs = ulmo.usgs.ned.get_raster_availability(dataset['layer'], dataset['bbox'])
+			assert len(locs['features'])==dataset['number_of_tiles']
+
+
+def test_get_raster():
+	layer = 'Alaska 2 arc-second'
+	bbox = (-149.5, 60.5, -149.3, 60.7)
+	path = tempfile.gettempdir()
+	availability_url = 'https://www.sciencebase.gov/catalog/items?fields=webLinks,spatial,title&q=&filter=tags=National Elevation Dataset (NED) Alaska 2 arc-second&filter=tags=IMG&filter=spatialQuery=Polygon ((-149.5 60.7,-149.5 60.5,-149.3 60.5,-149.3 60.7,-149.5 60.7))&format=json&max=1000'
+	zip_url = 'ftp://rockyftp.cr.usgs.gov/vdelivery/Datasets/Staged/NED/2/IMG/n61w150.zip'
+	url_files = {
+		availability_url: 'usgs/ned/get_raster_test_availability.json',
+		zip_url: 'n61w150.zip',
+	}
+
+	with test_util.mocked_urls(url_files):
+		locs = ulmo.usgs.ned.get_raster(layer, bbox, path=path)
+		raster_tile = locs['features'][0]['properties']['file']
+		assert filecmp.cmp(raster_tile, 'files/usgs/ned/n61w150.img')
