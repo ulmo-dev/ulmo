88a6669516aabc43d46416075bcf5e68d3a0251f
Dharhas Pothina <dharhas@gmail.com>
2015-07-02 09:51:31 -0500

    dropping python 2.6 support. tests are failing and I don't have the bandwidth to track it down.

--- ulmo/util/raster.py
+++ ulmo/util/raster.py
@@ -58,7 +58,7 @@ def download_tiles(path, tile_urls, tile_fmt, check_modified=False):
 
 def extract_from_zip(zip_path, tile_path, tile_fmt):
     tile_path = os.path.splitext(tile_path)[0] + tile_fmt
-    with contextlib.closing(zipfile.ZipFile(zip_path)) as z:
+    with zipfile.ZipFile(zip_path) as z:
         fname = [x for x in z.namelist() if tile_fmt in x[-4:]][0]
         with open(tile_path, 'wb') as f:
             f.write(z.read(fname))
