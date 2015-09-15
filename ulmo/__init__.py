636c0410a55b0e7c057c41557e1845a5b3253e3f
Dharhas Pothina <dharhas@gmail.com>
2015-09-15 15:50:16 -0500

    add pep 0440 version string

--- ulmo/__init__.py
+++ ulmo/__init__.py
@@ -4,7 +4,10 @@
 
     an open source library for clean, simple and fast access to public hydrology and climatology data
 """
-from __future__ import absolute_import
+from __future__ import (absolute_import, division, print_function)
+
+# ulmo version PEP-0440
+__version__ = '0.8.0'
 
 from . import cdec
 from . import cpc
