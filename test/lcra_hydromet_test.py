8f6860a689bca4f3cdec9bfa183d475e099ba38f
Solomon Negusse <solomon.negusse@twdb.texas.gov>
2015-12-10 15:26:53 -0600

    added tests for hydromet current data service

--- test/lcra_hydromet_test.py
+++ test/lcra_hydromet_test.py
@@ -6,10 +6,10 @@ import ulmo
 import test_util
 
 
-def test_get_sites():
+def test_get_sites_by_type():
     sites_file = 'lcra/hydromet/stream_stage_and_flow_sites_list.html'
     with test_util.mocked_urls(sites_file):
-        sites = ulmo.lcra.hydromet.get_sites('stage')
+        sites = ulmo.lcra.hydromet.get_sites_by_type('stage')
         assert 60 <= len(sites) <= 70
         assert '5499' in sites
 
@@ -29,3 +29,29 @@ def test_get_site_data():
     assert site_data.shape[0] == 2932
     are_equal = test_values == site_data.ix[test_values.index]
     assert are_equal.sum().sum() == 4
+
+
+def test_get_current_data():
+    test_values = pandas.DataFrame(
+        [{'datetime': datetime.datetime(2015, 12, 10, 14, 10),
+          'location': 'Barton Creek at Loop 360, Austin',
+          'stageft': 3.33,
+          'flowfs': 60.00,
+          'floodstageft': 8.00,
+          'bankfullstageft': 8.00
+          },
+          {'datetime': datetime.datetime(2015, 12, 10, 14, 10),
+           'location': 'Colorado River at Columbus',
+           'stageft': 10.32,
+           'flowfs': 975.00,
+           'bankfullstageft': 30.00,
+           'floodstageft': 34.00}])
+    test_values.set_index('location', inplace=True)
+    data_file = 'lcra/hydromet/current_data_2015-12-10-14-10.xml'
+    with test_util.mocked_urls(data_file):
+        current_data = ulmo.lcra.hydromet.get_current_data('getlowerbasin')
+        current_data_df = pandas.DataFrame(current_data)
+        current_data_df.set_index('location', inplace=True)
+    are_equal = test_values.values == current_data_df.ix[test_values.index].values
+    assert are_equal.sum() == 10
+    assert len(current_data) == 33
