548a62f0c9623efc96a9c950769a180058567bce
Dharhas Pothina <dharhas@gmail.com>
2019-02-04 14:06:10 -0600

    fix tests

--- test/wof_test.py
+++ test/wof_test.py
@@ -43,11 +43,11 @@ def test_core_get_site_info_waterml_1_1():
     with test_util.mocked_suds_client('1.1', dict(GetSiteInfo=site_info_file)):
         site_info = ulmo.cuahsi.wof.get_site_info(WSDL_URL, 'ipswich:MMB')
     assert site_info['code'] == 'MMB'
-    assert site_info['network'] == 'ipswich'
-    assert 'ipswich:Temp' in site_info['series']
-    assert 'ipswich:DO' in site_info['series']
-    assert len(site_info['series']) == 3
-    site_info['series']['ipswich:Temp']['variable']['units'] == {
+    assert site_info['network'] == 'IRWA'
+    assert 'IRWA:Temp' in site_info['series']
+    assert 'IRWA:DO' in site_info['series']
+    assert len(site_info['series']) == 4
+    site_info['series']['IRWA:Temp']['variable']['units'] == {
         'abbreviation': 'degC',
         'code': '96',
         'name': 'degree celsius',
@@ -116,13 +116,13 @@ def test_core_get_values_waterml_1_1():
     get_values_file = 'cuahsi/wof/get_values_1_1_ipswich_MMB_ipswich_Temp.xml'
     with test_util.mocked_suds_client('1.1', dict(GetValues=get_values_file)):
         values = ulmo.cuahsi.wof.get_values(WSDL_URL, 'ipswich:MMB', 'ipswich:Temp')
-    assert len(values['values']) == 112
+    assert len(values['values']) == 198
     assert values['values'][0] == {
         'censor_code': 'nc',
-        'date_time_utc': '1997-08-24T15:35:00.383',
-        'datetime': '1997-08-24T09:35:00',
-        'method_code': '2',
-        'quality_control_level_code': '3',
+        'date_time_utc': '1997-08-24T06:00:00',
+        'datetime': '1997-08-24T00:00:00',
+        'method_code': '21',
+        'quality_control_level_code': '1',
         'source_code': '1',
         'time_offset': '-06:00',
         'value': '19',
@@ -133,33 +133,8 @@ def test_core_get_values_waterml_1_1():
             'description': 'not censored',
         }
     }
-    assert values['methods'] == {
-        '2': {
-            'code': '2',
-            'description': '6" Glass Thermometer, Centigrade Scale',
-            'id': '2',
-        }
-    }
-    assert values['sources'] == {
-        '1': {
-            'abstract': 'The Ipswich River Watershed Associations (IRWA) RiverWatch water quality monitoring program, in operation since 1997, enlists volunteers to collect data on the health of the Ipswich River and its tributaries. Volunteers monitor according to a state-approved monitoring plan that ensures that the data they collect is of good quality. The goals of the program are to document River conditions in order to identify water quality problems (including adequate river flow) and to use this data to develop solutions to observed problems.',
-            'address': 'P.O. Box 576, 143 County Rd.\n,Ipswich, MA 01938-2557',
-            'citation': 'RiverWatch Water Quality Volunteer Monitoring Program Annual Results Reports. http://ipswichriver.org/resources/monitoring-data/',
-            'code': '1',
-            'contact_name': 'Ryan ODonnell',
-            'description': 'The Ipswich River Watershed Association (IRWA) is the voice of the Ipswich River. IRWA works to protect nature and make sure that there is enough clean water for people, fish and wildlife, today and for our children and theirs.',
-            'email': 'rodonnell@ipswichriver.org',
-            'id': '1',
-            'link': 'http://www.ipswichriver.org',
-            'metadata_link': 'http://ipswich-river.org/resources/monitoring-data/',
-            'organization': 'Ipswich River Watershed Association',
-            'phone': '978-412-8200',
-            'profile_version': 'Unknown',
-            'title': 'Ipswich River RiverWatch Program',
-            'topic_category': 'inlandWaters',
-            'type_of_contact': 'main',
-        }
-    }
+    assert values['methods'] == {'21': {'code': '21', 'description': '6"" Glass Thermometer, Centigrade Scale', 'id': '21'}}
+    assert values['sources'] == {'1': {'id': '1', 'code': '1', 'organization': 'Ipswich River Watershed Association', 'description': 'The Ipswich River Watershed Association (IRWA) is the voice of the Ipswich River.� IRWA works to protect nature and make sure that there is enough clean water for people, fish and wildlife, today and for our children and theirs.', 'topic_category': 'inlandWaters', 'title': 'Ipswich River RiverWatch Program', 'abstract': 'The RiverWatch program, in operation since 1997, enlists a group of volunteers to collect water quality data on the Ipswich River and its tributaries. The purpose of the program is to establish baseline data in order to identify and address impairments to water quality and quantity, as well as to promote awareness and stewardship of the river. Volunteers monitor according to a state-approved monitoring plan that ensures that the data collected is of good quality. The goals of the program are to document River conditions in order to identify water quality problems (including adequate river flow) and to use this data to develop solutions to observed problems. A Quality Assurance Project Plan (QAPP) was finalized in 1999 and most recently updated in 2013. ', 'profile_version': 'Unknown', 'metadata_link': 'http://ipswich-river.org/resources/monitoring-data/', 'contact_name': "Ryan O'Donnell", 'type_of_contact': 'main', 'email': 'rodonnell@ipswichriver.org', 'phone': '978-412-8200', 'address': 'P.O. Box 576,143 County Rd.\n,Ipswich, MA 01938-2557', 'link': 'http://www.ipswichriver.org', 'citation': 'RiverWatch Water Quality Volunteer Monitoring Program Annual Results Reports. http://ipswichriver.org/resources/monitoring-data/'}}
 
 
 def test_core_get_variable_info_waterml_single_1_0():
@@ -231,34 +206,7 @@ def test_core_get_variable_info_waterml_single_1_1():
     with test_util.mocked_suds_client('1.1', dict(GetVariableInfo=get_variable_info_file)):
         variable_info = ulmo.cuahsi.wof.get_variable_info(WSDL_URL, 'ipswich:Temp')
 
-    assert variable_info == {
-        'code': 'Temp',
-        'data_type': 'Sporadic',
-        'general_category': 'Water Quality',
-        'id': '3',
-        'name': 'Temperature',
-        'no_data_value': '-9999',
-        'sample_medium': 'Surface Water',
-        'speciation': 'Not Applicable',
-        'units': {
-            'abbreviation': 'degC',
-            'code': '96',
-            'name': 'degree celsius',
-            'type': 'Temperature',
-        },
-        'time': {
-            'interval': '0',
-            'is_regular': True,
-            'units': {
-                'abbreviation': 'month',
-                'code': '106',
-                'name': 'month',
-                'type': 'Time',
-            },
-        },
-        'value_type': 'Sample',
-        'vocabulary': 'ipswich',
-    }
+    assert variable_info == {'value_type': 'Sample', 'data_type': 'Sporadic', 'general_category': 'Water Quality', 'sample_medium': 'Surface Water', 'no_data_value': '-9999', 'speciation': 'Not Applicable', 'code': 'Temp', 'id': '3', 'name': 'Temperature', 'vocabulary': 'IRWA', 'time': {'is_regular': True, 'interval': '0', 'units': {'abbreviation': 'month', 'code': '106', 'name': 'month', 'type': 'Time'}}, 'units': {'abbreviation': 'degC', 'code': '96', 'name': 'degree celsius', 'type': 'Temperature'}}
 
 
 def test_core_get_variable_info_waterml_all_1_1():
@@ -268,38 +216,11 @@ def test_core_get_variable_info_waterml_all_1_1():
         variable_info = ulmo.cuahsi.wof.get_variable_info(WSDL_URL)
 
     check_includes = [
-        'ipswich:DO',
-        'ipswich:PercDO',
-        'ipswich:Temp',
+        'IRWA:DO',
+        'IRWA:PercDO',
+        'IRWA:Temp',
     ]
     for check in check_includes:
         assert check in variable_info
 
-    assert variable_info['ipswich:Temp'] == {
-        'code': 'Temp',
-        'data_type': 'Sporadic',
-        'general_category': 'Water Quality',
-        'id': '3',
-        'name': 'Temperature',
-        'no_data_value': '-9999',
-        'sample_medium': 'Surface Water',
-        'speciation': 'Not Applicable',
-        'units': {
-            'abbreviation': 'degC',
-            'code': '96',
-            'name': 'degree celsius',
-            'type': 'Temperature',
-        },
-        'time': {
-            'interval': '0',
-            'is_regular': True,
-            'units': {
-                'abbreviation': 'month',
-                'code': '106',
-                'name': 'month',
-                'type': 'Time',
-            },
-        },
-        'value_type': 'Sample',
-        'vocabulary': 'ipswich',
-    }
+    assert variable_info['IRWA:Temp'] == {'value_type': 'Sample', 'data_type': 'Sporadic', 'general_category': 'Water Quality', 'sample_medium': 'Surface Water', 'no_data_value': '-9999', 'speciation': 'Not Applicable', 'code': 'Temp', 'id': '3', 'name': 'Temperature', 'vocabulary': 'IRWA', 'time': {'is_regular': True, 'interval': '0', 'units': {'abbreviation': 'month', 'code': '106', 'name': 'month', 'type': 'Time'}}, 'units': {'abbreviation': 'degC', 'code': '96', 'name': 'degree celsius', 'type': 'Temperature'}}
